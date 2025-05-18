b8aa77d72ee6350041910bbf6fc03947af0662c9
Brian Blosser <bpb54321@gmail.com>
2025-05-18 16:42:23 -0400

    Convert App to a function component.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createRoot } from "./react-dom-client.development.js";
 import * as React from "./react.development.js";
 
-const App = React.createElement("h1", null, "Hello world!");
+function App() {
+  console.log("App called");
+  return React.createElement("h1", null, "Hello world!");
+}
 
 const rootElement = document.getElementById("root");
 const reactRoot = createRoot(rootElement);
-reactRoot.render(App);
+reactRoot.render(React.createElement(App));
