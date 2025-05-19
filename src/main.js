b8a42248ce715edbf727072907e1957ef07539c1
Brian Blosser <bpb54321@gmail.com>
2025-05-19 15:52:25 -0400

    Return basic tree of react elements with children.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { createRoot } from "./production/react-dom-client.production.js";
-import * as React from "./production/react.production.js";
+import { createElement } from "./production/react.production.js";
 
 function App() {
-  console.log("App called");
-  return React.createElement("h1", null, "Hello world!");
+  return createElement(
+    "article",
+    null,
+    createElement("h2", null, "Counter"),
+    createElement("p", null, "Button pressed 1 times"),
+    createElement("button", null, "Increment"),
+  );
 }
 
 const rootElement = document.getElementById("root");
 const reactRoot = createRoot(rootElement);
-reactRoot.render(React.createElement(App));
+reactRoot.render(createElement(App));
